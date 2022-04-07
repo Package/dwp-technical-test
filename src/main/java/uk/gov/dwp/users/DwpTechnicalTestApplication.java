6857fefb7d8696d03f71a30734da1408edfd7144
Package <ryan.dean1000@outlook.com>
2022-04-07 16:53:22 +0100

    Initial commit: scaffold Spring Boot project

---
+++ src/main/java/uk/gov/dwp/users/DwpTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.dwp.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwpTechnicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwpTechnicalTestApplication.class, args);
+    }
+
+}
